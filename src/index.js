42b46711d73a650d0c5d68afd5acf6595bac1e80
Pedro <thepeterace@gmail.com>
2022-04-21 12:58:47 -0400

    ToDo

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// import { saludar } from './js/componentes';
+import './styles.css';
+
+import { Todo, TodoList } from './classes';
+import { crearTodoHTML } from './js/componentes';
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach( crearTodoHTML );
+
+console.log('Todo: ', todoList.todos );
+
+
